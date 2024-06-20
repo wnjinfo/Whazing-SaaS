607e7f2667f0217f7612eea46e4b029f7dcc4ad9
cleitonme <cleitonme@gmail.com>
2024-06-20 10:09:54 -0300

    Delete vue-i18n-extract.config.js

--- frontend/vue-i18n-extract.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	  input: './src', // Diretório de entrada onde estão seus arquivos de código-fonte
-	  output: './src/locales', // Diretório de saída onde os arquivos de tradução serão gerados
-	  locales: ['pt', 'en', 'es'], // Lista de idiomas suportados
-	  extension: '.json' // Extensão dos arquivos de tradução gerados
-	  vueFiles: ["**/*.vue"]
-
-
-};
