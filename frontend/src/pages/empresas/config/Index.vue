a7b96de744111df366030a46ff04033c18ce5d3c
cleitonme <cleitonme@gmail.com>
2024-06-28 17:43:07 -0300

    solicitacao teste automatico

---
+++ frontend/src/pages/empresas/config/Index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <q-list class="text-weight-medium">
+      <q-item-label header class="text-bold text-h6 q-mb-lg">Configurações</q-item-label>
+
+      <q-item-label caption class="q-mt-lg q-pl-sm">Módulo: SaaS</q-item-label>
+      <q-separator spaced />
+
+      <q-item tag="label" v-ripple>
+        <q-item-section>
+          <q-item-label>Permitir solicitação de teste</q-item-label>
+          <q-item-label caption>Habilitando essa opção poderá ser feito a solicitação de teste automatico</q-item-label>
+        </q-item-section>
+        <q-item-section avatar>
+          <q-toggle v-model="allowSignup" false-value="disabled" true-value="enabled" checked-icon="check"
+            keep-color :color="allowSignup === 'enabled' ? 'green' : 'negative'" size="md"
+            unchecked-icon="clear" @input="atualizarConfiguracao('allowSignup')" />
+        </q-item-section>
+      </q-item>
+
+      <div class="row q-px-md" v-if="allowSignup === 'enabled'">
+        <div class="col-12">
+          <q-input v-model="timeTest" type="number" autogrow dense outlined
+            label="Tempo em dias teste:" input-style="min-height: 6vh; max-height: 9vh;" debounce="700"
+            @input="atualizarConfiguracao('timeTest')" />
+        </div>
+      </div>
+
+    </q-list>
+
+  </div>
+</template>
+
+<script>
+import { ListarConfiguracoes, AlterarConfiguracao } from '../../../service/configuracoes'
+export default {
+  name: 'ConfiguracoesSaaS',
+  data() {
+    return {
+      configuracoes: [],
+      allowSignup: null,
+      timeTest: ''
+    }
+  },
+  methods: {
+    async listarConfiguracoes() {
+      const { data } = await ListarConfiguracoes()
+      this.configuracoes = data
+      this.configuracoes.forEach(el => {
+        const value = el.value
+        this.$data[el.key] = value
+      })
+    },
+    async atualizarConfiguracao(key) {
+      const params = {
+        key,
+        value: this.$data[key]
+      }
+      try {
+        await AlterarConfiguracao(params)
+        this.$q.notify({
+          type: 'positive',
+          message: 'Configuração alterada!',
+          progress: true,
+          actions: [{
+            icon: 'close',
+            round: true,
+            color: 'white'
+          }]
+        })
+      } catch (error) {
+        console.error('error - AlterarConfiguracao', error)
+        this.$data[key] = this.$data[key] === 'enabled' ? 'disabled' : 'enabled'
+        this.$notificarErro('Ocorreu um erro!', error)
+      }
+    }
+  },
+  async mounted() {
+    await this.listarConfiguracoes()
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
